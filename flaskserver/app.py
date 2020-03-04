9ceb8877b057de169ed496be6b5ac92237d2ab8d
ElmerReal <elmerreal@hotmail.com>
2020-03-04 14:05:07 -0600

    Dockercompose file added

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -8,14 +8,18 @@ r = redis.Redis(host=ENV_HOST, port=ENV_PORT, db=ENV_DB)
 r.mset({"Jhon": "22 years", "Jhon": "30 years"})
 app = Flask(__name__)
 
-@app.route('/Guardar/<username>/<age>')
+@app.route('/flask/Guardar/<username>/<age>')
 def hello(username,age):
     r.set(username,age)
     return " [ "+username+" ] Guardado correctamente"
 
-@app.route('/Listar')
+@app.route('/flask/Listar')
 def hello1():
     return str(r.keys("*"))
+
+@app.route('/flask/')
+def hello2s():
+    return "Bienvenido a la aplicacion python"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
