1e73d812cf935334cca5edad1cd1382524832a0d
ElmerReal <elmerreal@hotmail.com>
2020-03-04 12:33:00 -0600

    Dockercompose file added

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import redis
+import os
+USER = os.getenv('ENV_USER')
+r = redis.Redis(host='localhost', port=6379, db=0)
 #from flask_redis import Redis
 app = Flask(__name__)
 #redis = Redis(app)
 
 @app.route('/')
 def hello():
-    return "Hello World- Trambo!"
+    return "Hello World- Trambo!-> ENV   "+USER
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
